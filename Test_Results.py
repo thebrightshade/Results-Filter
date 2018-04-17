1490b7340eb3b26a8187027dda229ae36c750ec1
Barday <shadmaan.barday@hp.com>
2018-04-17 12:39:34 -0700

    fixed typo in the initial comment

--- Test_Results.py
+++ Test_Results.py
@@ -5,7 +5,7 @@ Created on Wed Apr 04 18:43:05 2018
 @author: barday
 
 The purpose of this script is to go through the log files that are generated during the Automated Karnak Testing and provide a quick summary of Pass/Fail results.
-The script must be called in this format: Karnak_Results.py C:\Users\Demo_Path\ """
+The script must be called in this format: Test_Results.py C:\Users\Demo_Path\ """
 
 import os
 import sys
